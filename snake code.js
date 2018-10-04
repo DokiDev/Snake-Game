051498150543e9da8a10bbc29922cc83466cefc4
DokiDev <31711314+DokiDev@users.noreply.github.com>
2018-10-04 12:46:18 -0400

    Added World, SnakeController, and Player classes

--- snake code.js
+++ snake code.js
@@ -1,24 +1,122 @@
+class Point {
+  constructor(a,b) {
+    this.x_ = a;
+    this.y_ = b;
+  }
+  get x(){
+    return this.x_;
+  }
+  get y(){
+    return this.y_;
+  }
+}
 class Snake {
-  constructor(startPos) {
-    this.moveForward_ = true;
-    this.x = startPos;
+  constructor() {
+    this.position_ = new Point(0,0);
+    this.direction_ = "left";
   }
-  move(dist){
-    if(this.moveForward_) this.x += dist;
-    else this.x -= dist;
+  move(amount){
+    if(this.direction_ === "left") {
+      this.position_ = new Point(this.position_.x - amount, this.position_.y);
+    }
+    else if(this.direction_ === "right") {
+      this.position_ = new Point(this.position_.x + amount, this.position_.y);
+    }
+    else if(this.direction_ === "up") {
+      this.position_ = new Point(this.position_.x, this.position_.y - amount);
+    }
+    else if(this.direction_ === "down") {
+      this.position_ = new Point(this.position_.x, this.position_.y + amount);
+    }
   }
-  turn(){
-    if(this.moveForward_) this.moveForward_ = false;
-    else this.moveForward_ = true;
+  turnLeft(){
+    if(this.direction_ === "left") this.direction_ = "down";
+    else if(this.direction_ === "down") this.direction_ = "right";
+    else if(this.direction_ === "right") this.direction_ = "up";
+    else this.direction === "left";
+    console.log("Now facing " + this.direction_);
+  }
+  turnRight(){
+    if(this.direction_ === "left") this.direction_ = "up";
+    else if(this.direction_ === "up") this.direction_ = "right";
+    else if(this.direction_ === "right") this.direction_ = "down";
+    else this.direction === "left";
+    console.log("Now facing " + this.direction_);
   }
   get currentPos(){
-    return this.x;
+    return this.position_;
+  }
+  get direction(){
+    return this.direction_;
   }
 }
 
-let boa = new Snake(0);
+class World {
+  constructor(assignSnake){
+    this.worldSnake_ = assignSnake;
+    this.width_ = 320;
+    this.height_ = 480;
+  }
+  update(steps){
+    this.worldSnake_.move(steps);
+  }
+  get snake(){
+    return this.worldSnake_;
+  }
+  get width(){
+    return this.width_;
+  }
+  get height(){
+    return this.height_;
+  }
+}
 
-boa.move(1);
-boa.turn();
-boa.move(3);
-console.log(boa.x);
+class SnakeController {
+  constructor(world, snake){
+    this.snakeWorld_ = world;
+    this.slitherer_ = snake;
+  }
+  turnSnakeLeft(){
+    this.slitherer_.turnLeft();
+  }
+  turnSnakeRight(){
+    this.slitherer_.turnRight();
+  }
+  get snakePos(){
+    return this.slitherer_.currentPos;
+  }
+  get snakeDir(){
+    return this.slitherer_.direction;
+  }
+  get worldWidth(){
+    return snakeWorld_.width;
+  }
+  get worldHeight(){
+    return snakeWorld_.height;
+  }
+}
+
+class Player {
+  constructor(snakeCont){
+    if(this.constructor === Player) throw new Error("Cannot instantiate a Player, which is an abstract base class.");
+    if(!(this.makeTurn instanceof Function)) throw new Error("Base class must implement makeTurn method.");
+    this.sc_ = snakeCont;
+  }
+}
+
+class AvoidWallsPlayer extends Player {
+  constructor(){
+    super();
+  }
+  makeTurn(){
+    if((this.sc_.snakeDir === "left") && (this.sc_.snakePos.x === 0)) {
+      this.sc_.turnSnakeLeft();
+    }
+  }
+}
+
+let solidSnake = new Snake();
+let helloWorld = new World(solidSnake);
+let nesController = new SnakeController(helloWorld, solidSnake);
+let player1 = new AvoidWallsPlayer(nesController);
+
